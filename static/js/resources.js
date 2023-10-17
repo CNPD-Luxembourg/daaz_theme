88461b25bfb27e2b3652c56f156d02c6077b7d3b
Juan Rocha <juan.rocha@nc3.lu>
2023-10-17 16:53:59 +0200

    [template]Added resources download

--- static/js/resources.js
+++ static/js/resources.js
@@ -1,9 +1,25 @@
 $(document).ready(function () {
+    $('.go-to-download').click(function() {
+        var $popup = $("#resources_download");
+        var popup_url = 'resources_download/';
+        if ($(this).data("level-id")){
+            var popup_url = 'resources_download/' + '?level='+ $(this).data("level-id");
+        }
+        if ($(this).data("rt-id")){
+            var popup_url = 'resources_download/' + '?resource_type='+ $(this).data("rt-id");
+        }
+
+        $(".modal-body", $popup).load(popup_url, function () {
+            $popup.modal("show");
+        });
+    });
+
+
     $('#CheckAll').change(function (e) {
         if (e.currentTarget.checked) {
-            $('.option').find('input[type="checkbox"]').prop('checked', true);
+            $('.option').find('input[type="checkbox"]').not(":disabled").prop('checked', true);
         } else {
-            $('.option').find('input[type="checkbox"]').prop('checked', false);
+            $('.option').find('input[type="checkbox"]').not(":disabled").prop('checked', false);
         }
     });
 });
