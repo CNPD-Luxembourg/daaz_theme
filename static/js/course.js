39677be9b9dc361744a42dbeff2db8189b812cab
Juan Rocha <juan.rocha@nc3.lu>
2023-11-09 15:25:13 +0100

    [course]Improved layout

--- static/js/course.js
+++ static/js/course.js
@@ -15,6 +15,17 @@ $(document).ready(function () {
         submitButton.attr("disabled", !checkboxesAndRadios.is(":checked"));
     }
 
+    $('.previous_slide').click(function () {
+        var $divId = $("#carouselCourse");
+        var $progress_bar_id = $(".course-progress-bar");
+        var url = 'previous_slide/';
+        $($divId).load(url, function () {
+            $($progress_bar_id).fadeOut(500)
+            $($progress_bar_id).load("update_progress_bar/")
+            $($progress_bar_id).fadeIn(100);
+        });
+    });
+
     $('.next_slide').click(function () {
         var $divId = $("#carouselCourse");
         var $progress_bar_id = $(".course-progress-bar");
@@ -25,8 +36,4 @@ $(document).ready(function () {
             $($progress_bar_id).fadeIn(100);
         });
     });
-
-
-
-
 });
