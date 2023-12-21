42f2a50d37e546153019fe17d914ae2632845dcc
Juan Rocha <juan.rocha@nc3.lu>
2023-12-21 11:57:16 +0100

    [dashboard]Added first draft of new layout

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,73 +1,6 @@
 
 $(document).ready(function () {
-    let criteria = JSON.parse(document.getElementById('criteria-data').textContent);
     let progress = JSON.parse(document.getElementById('progress-data').textContent);
-    const ctx = document.getElementById('dashboard');
-    new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: criteria.labels,
-            datasets: [{
-                data: criteria.data,
-                borderRadius: 30,
-                borderSkipped: false,
-                backgroundColor: '#AFDFFF',
-                barPercentage: 0.6
-            },
-            {
-                data: criteria.data.map(() => { return 100 }),
-                borderRadius: 30,
-                backgroundColor: '#ECF6FF',
-                barPercentage: 0.6
-            }]
-        },
-        options: {
-            indexAxis: 'y',
-            scales: {
-                y: {
-                    ticks: {
-                        format: {
-                            style: 'percent',
-                            minimumFractionDigits: 0,
-                        },
-                        count: 5,
-
-                    },
-                    min: 0,
-                    max: 1,
-                    grid: {
-                        display: false,
-                        drawTicks: false,
-                        drawOnChartArea: false,
-                    },
-                    border: {
-                        display: false,
-                    },
-                },
-                x: {
-                    grid: {
-                        display: false,
-                    },
-                    border: {
-                        display: false,
-                    },
-                    ticks: {
-                        display: false,
-                        font: {
-                            weight: 'bold',
-                        },
-                        maxRotation: 90,
-                        minRotation: 30
-                    }
-                },
-            },
-            plugins: {
-                legend: {
-                    display: false
-                }
-            },
-        }
-    });
 
     progress.forEach((el, i) => {
         let index = i + 1;
@@ -78,23 +11,16 @@ $(document).ready(function () {
             data: {
                 labels: [],
                 datasets: [{
-                    data: [100 - progress, progress],
-                    backgroundColor: ['#006ED3', '#FFFFFF'],
-                    borderWidth: 0
+                    data: [progress, progress - 100],
+                    backgroundColor: ['#0099FF', '#FFFFFF'],
+                    borderWidth: 0,
+                    borderRadius: 10,
                 }]
             },
             options: {
                 responsive: true,
                 maintainAspectRatio: true,
-                cutout: "85%",
-                animations: {
-                    startAngle: {
-                      from: Math.PI * 2
-                    },
-                    endAngle: {
-                      from: Math.PI * 2
-                    }
-                },
+                cutout: "75%",
                 plugins: {
                     tooltip: {
                         enabled: false
