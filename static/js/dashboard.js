87467a6c0e45212390929ce678ef7aff79a1b636
Juan Rocha <juan.rocha@nc3.lu>
2024-01-11 16:54:09 +0100

    [dashboard]Improved responsive

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -2,6 +2,26 @@
 $(document).ready(function () {
     let progress = JSON.parse(document.getElementById('progress-data').textContent);
 
+    const doughnutLabel = {
+        id: 'doughnutLabel',
+        beforeDraw(chart){
+            let width = chart.width;
+            let height = chart.height + 10;
+            let {ctx, data} = chart;
+            let fontSize = (height/100).toFixed(2);
+            ctx.font = `bold ${fontSize}em Inter`;
+            ctx.textBaseline = "middle";
+
+            let text = `${data.datasets[0].data[0]}%`,
+                textX = Math.round((width - ctx.measureText(text).width) / 2),
+                textY = height / 2;
+
+            ctx.fillText(text, textX, textY);
+            ctx.save();
+
+        },
+    }
+
     progress.forEach((el, i) => {
         let index = i + 1;
         let progress = el;
@@ -19,14 +39,15 @@ $(document).ready(function () {
             },
             options: {
                 responsive: true,
-                maintainAspectRatio: true,
+                maintainAspectRatio: false,
                 cutout: "75%",
                 plugins: {
                     tooltip: {
                         enabled: false
                     }
                 },
-            }
+            },
+            plugins:[doughnutLabel]
         });
     });
 
