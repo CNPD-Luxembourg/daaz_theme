d2d4cda8574d59dfcdc8d9fa37717f599a48a3f9
Juan Rocha <juan.rocha@nc3.lu>
2024-01-15 16:41:33 +0100

    [charts]Improved dougnout charts

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,44 +1,56 @@
 
 $(document).ready(function () {
-    let progress = JSON.parse(document.getElementById('progress-data').textContent);
+    let progressValues = JSON.parse(document.getElementById('progress-data').textContent);
+    let successValues = JSON.parse(document.getElementById('success-data').textContent);
 
-    const doughnutLabel = {
-        id: 'doughnutLabel',
-        beforeDraw(chart){
-            let width = chart.width;
-            let height = chart.height + 10;
-            let {ctx, data} = chart;
-            let fontSize = (height/100).toFixed(2);
-            ctx.font = `bold ${fontSize}em Inter`;
-            ctx.textBaseline = "middle";
-
-            let text = `${data.datasets[0].data[0]}%`,
-                textX = Math.round((width - ctx.measureText(text).width) / 2),
-                textY = height / 2;
-
-            ctx.fillText(text, textX, textY);
-            ctx.save();
-
-        },
-    }
-
-    progress.forEach((el, i) => {
+    progressValues.forEach((value, i) => {
         let index = i + 1;
-        let progress = el;
-        let progress_ctx = document.getElementById('progress' + index)
-        new Chart(progress_ctx, {
+        let progress_sm_ctx = document.getElementById('progress_sm' + index);
+        doughnutChart(progress_sm_ctx, value, false)
+    });
+
+    successValues.forEach((value, i) => {
+        let index = i + 1;
+        let success_ctx = document.getElementById('success' + index);
+        doughnutChart(success_ctx, value)
+
+        let success_sm_ctx = document.getElementById('success_sm' + index);
+        doughnutChart(success_sm_ctx, value, false)
+    });
+
+    function doughnutChart(ctx, value, responsive=true) {
+        const doughnutLabel = {
+            id: 'doughnutLabel',
+            beforeDraw(chart){
+                let width = chart.width;
+                let height = chart.height + 10;
+                let {ctx, data} = chart;
+                let fontSize = (height/100).toFixed(2);
+                ctx.font = `bold ${fontSize}em Inter`;
+                ctx.textBaseline = "middle";
+
+                let text = `${data.datasets[0].data[0]}%`,
+                    textX = Math.round((width - ctx.measureText(text).width) / 2),
+                    textY = height / 2;
+
+                ctx.fillText(text, textX, textY);
+                ctx.save();
+
+            },
+        }
+        new Chart(ctx, {
             type: 'doughnut',
             data: {
                 labels: [],
                 datasets: [{
-                    data: [progress, progress - 100],
+                    data: [value, value - 100],
                     backgroundColor: ['#0099FF', '#ECF6FF'],
                     borderWidth: 0,
                     borderRadius: 10,
                 }]
             },
             options: {
-                responsive: true,
+                responsive: responsive,
                 maintainAspectRatio: false,
                 cutout: "75%",
                 events: [],
@@ -50,6 +62,7 @@ $(document).ready(function () {
             },
             plugins:[doughnutLabel]
         });
-    });
+
+    }
 
 });
