87cf3504702c33c9b9c63caca26b7880a3485923
Juan Rocha <juanferocha@hotmail.com>
2024-03-11 13:40:41 +0100

    [stats]Added users by level

--- static/js/stats.js
+++ static/js/stats.js
@@ -1,19 +1,25 @@
 
 $(document).ready(function () {
-    let usersData = JSON.parse(document.getElementById('users-data').textContent);
-    let dates = usersData.map(user => new Date(user.created_at))
-    let user_values = usersData.map(user => user.total_users)
+    let usersBydate = JSON.parse(document.getElementById('users_by_date').textContent);
+    let usersByLevel = JSON.parse(document.getElementById('users_by_level').textContent);
+    let user_dates = usersBydate.map(user => new Date(user.created_at))
+    let user_dates_values = usersBydate.map(user => user.total_users)
+    let user_levels = usersByLevel.map(user => user.current_level__index)
+    let user_levels_values = usersByLevel.map(user => user.total_users)
+    console.log(usersByLevel);
 
-    let users_bar_ctx = document.getElementById('users_stats_bar');
-    let users_line_ctx = document.getElementById('users_stats_line');
-    let users_data = user_values;
-    
-    barChart(users_bar_ctx,users_data)
-    lineChart(users_line_ctx,users_data)
+    let users_by_date_bar_ctx = document.getElementById('users_by_date_bar');
+    let users_by_date_line_ctx = document.getElementById('users_by_date_line');
+    let users_by_level_bar_ctx = document.getElementById('users_by_level_bar');
+    let users_by_level_line_ctx = document.getElementById('users_by_level_line');
+
+    barChart(users_by_date_bar_ctx,user_dates,user_dates_values,'time')
+    lineChart(users_by_date_line_ctx,user_dates,user_dates_values, 'time')
+    barChart(users_by_level_bar_ctx,user_levels,user_levels_values)
+    lineChart(users_by_level_line_ctx,user_levels,user_levels_values)
 
 
-    function barChart(ctx,values) {
-        const labels = dates;
+    function barChart(ctx,labels,values,typeXaxis='category') {
         new Chart(ctx, {
             type: 'bar',
             data: {
@@ -31,7 +37,7 @@ $(document).ready(function () {
                 responsive: true,
                 scales: {
                     x: {
-                        type: 'time',
+                        type: typeXaxis,
                     },
                     y: {
                         beginAtZero: true,
@@ -44,8 +50,7 @@ $(document).ready(function () {
         });
     }
 
-    function lineChart(ctx,values) {
-        const labels = dates;
+    function lineChart(ctx,labels,values,typeXaxis='category') {
         new Chart(ctx, {
             type: 'line',
             data: {
@@ -63,7 +68,7 @@ $(document).ready(function () {
                 responsive: true,
                 scales: {
                     x: {
-                        type: 'time',
+                        type: typeXaxis,
                     },
                     y: {
                         beginAtZero: true,
