bec626d072505d28557487ba1fc878da313a337d
Juan Rocha <juan.rocha@nc3.lu>
2024-03-12 11:47:06 +0100

    [stats]Added github chart

--- static/js/stats.js
+++ static/js/stats.js
@@ -5,61 +5,32 @@ $(document).ready(function () {
     let scoreBylevel = JSON.parse(document.getElementById('avg_score_by_level').textContent);
     let progressBylevel = JSON.parse(document.getElementById('avg_progress_by_level').textContent);
 
-    let user_dates = usersBydate.map(user => new Date(user.created_at))
     let user_levels = usersByLevel.map(user => user.current_level__index)
-    let user_dates_values = usersBydate.map(user => user.total_users)
-    let user_levels_values = usersByLevel.map(user => user.total_users)
+    let user_levels_values = usersByLevel.map(user => user.count)
     let score_levels_values = scoreBylevel.map(user => user.avg_score/100)
     let progress_levels_values = progressBylevel.map(user => user.avg_progress/100)
 
-    const users_by_date_bar_ctx = document.getElementById('users_by_date_bar');
-    const users_by_date_line_ctx = document.getElementById('users_by_date_line');
-    const users_by_level_bar_ctx = document.getElementById('users_by_level_bar');
-    const users_by_level_line_ctx = document.getElementById('users_by_level_line');
-    const users_and_score_by_level_bar_ctx = document.getElementById('users_and_score_by_level_bar');
-    const users_and_score_by_level_line_ctx = document.getElementById('users_and_score_by_level_line');
-    const users_and_progress_by_level_bar_ctx = document.getElementById('users_and_progress_by_level_bar');
-    const users_and_progress_by_level_line_ctx = document.getElementById('users_and_progress_by_level_line');
+    const users_by_level_ctx = document.getElementById('users_by_level_chart');
+    const users_and_score_by_level_ctx = document.getElementById('users_and_score_by_level_chart');
+    const users_and_progress_by_level_ctx = document.getElementById('users_and_progress_by_level_chart');
+
+
+    $('#surveys-activity').github_graph({
+        data: usersBydate ,
+        texts: ['user','users'],
+      });
 
 
     drawSingleAxisChart(
         'bar',
-        users_by_date_bar_ctx,
-        user_dates, 
-        user_dates_values, 
-        'time'
-    )
-    drawSingleAxisChart(
-        'line', 
-        users_by_date_line_ctx,
-        user_dates, 
-        user_dates_values, 
-        'time'
-    )
-    drawSingleAxisChart(
-        'bar',
-        users_by_level_bar_ctx, 
+        users_by_level_ctx,
         user_levels,
         user_levels_values
     )
-    drawSingleAxisChart(
-        'line',
-        users_by_level_line_ctx, 
-        user_levels,
-        user_levels_values
-    )
+
     drawMultipleAxisChart(
         'bar',
-        users_and_score_by_level_bar_ctx,
-        user_levels,
-        'Users',
-        user_levels_values,
-        'Average score',
-        score_levels_values
-    )
-    drawMultipleAxisChart(
-        'line',
-        users_and_score_by_level_line_ctx,
+        users_and_score_by_level_ctx,
         user_levels,
         'Users',
         user_levels_values,
@@ -68,16 +39,7 @@ $(document).ready(function () {
     )
     drawMultipleAxisChart(
         'bar',
-        users_and_progress_by_level_bar_ctx,
-        user_levels,
-        'Users',
-        user_levels_values,
-        'Average progress',
-        progress_levels_values
-    )
-    drawMultipleAxisChart(
-        'line',
-        users_and_progress_by_level_line_ctx,
+        users_and_progress_by_level_ctx,
         user_levels,
         'Users',
         user_levels_values,
@@ -128,7 +90,7 @@ $(document).ready(function () {
                         yAxisID: 'y',
                         order: 1
                     },
-                    {   
+                    {
                         label: legendY2axis,
                         data: valuesY2axis,
                         yAxisID: 'y2',
